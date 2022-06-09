9e0c80a2aba4b7f96866d6792e7fecd9492ce886
vyshnavipendru <99222484+vyshnavipendru@users.noreply.github.com>
2022-06-09 11:12:43 -0400

    split test and train data

--- census.py
+++ census.py
@@ -27,13 +27,39 @@ print(data[categorical].head())
 
 
 # all unique values in categorical data
-print("\nall unique values in categorical data: \n",np.unique(data[categorical].values))
+# print("\nall unique values in categorical data: \n",np.unique(data[categorical].values))
 # unique values in each categorical data and frequecy distribution of values and 
 # replacing '?' with 'NaN'
 print("\nunique values in each categorical data and frequecy distribution of values: \n")
 for col in categorical:
-    print(col,data[col].unique())
-    print(data[col].value_counts())
+    print("\nUnique row names in Column: ",col,data[col].unique())
+    print("Frequency of values: \n",data[col].value_counts())
+    data[col].replace('?',np.NaN,inplace=True)
     data[col].replace(' ?',np.NaN,inplace=True)
 
-print("\nall unique values in categorical data: \n",np.unique(data[categorical].values))
+
+# print("unique in occupation \n",data.occupation.value_counts())
+
+print("\nNumber of missing values:",data[categorical].isnull().sum())
+#workclass,occuption,native_country has missing values
+
+for col in categorical:
+    print(col,'contains ',len(data[col].unique()),' labels')
+
+#Segregate numerical variables 
+numerical= [v for v in data.columns if data[v].dtype!='O']
+print('\nThe numberical variables :', numerical)
+#only cloumns of numerical data
+print('\n',data[numerical].head())
+
+print("\nNumber of missing values:",data[numerical].isnull().sum())
+#no missing values
+
+#declaring features and labels
+X=data.drop(['income'],axis=1)
+y=data['income']
+
+#splitting test and train data
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
